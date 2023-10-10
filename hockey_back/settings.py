75cd03e65b1e41dbdc9e931117beae0d7342ab6e
Shoichii <khayn1992@gmail.com>
2023-10-10 16:59:06 +0300

    version 2.0

--- hockey_back/settings.py
+++ hockey_back/settings.py
@@ -124,15 +124,8 @@ WSGI_APPLICATION = 'hockey_back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG == 'False':
     ALLOWED_HOSTS = ['78.40.217.95', 'hockeybot.tw1.su']
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
     DATABASES = {
         'default': {
