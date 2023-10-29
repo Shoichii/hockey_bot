cf7b10143c35fe9427e49854edac5e8741c99b64
Shoichii <khayn1992@gmail.com>
2023-10-29 14:06:01 +0300

    fix

--- shedule_app/management/commands/bot.py
+++ shedule_app/management/commands/bot.py
@@ -56,5 +56,4 @@ class Command(BaseCommand):
                 if message.media_group_id and message.conf.get("is_last"):
                     del self.album_data[message.media_group_id]
         dp.middleware.setup(AlbumMiddleware())
-        if __name__ == '__main__':
-            executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
+        executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
