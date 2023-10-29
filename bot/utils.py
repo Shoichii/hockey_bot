69de40f34ed3e594577407ffd75d683dc73a99ab
Shoichii <khayn1992@gmail.com>
2023-10-29 14:23:23 +0300

    fix

--- bot/utils.py
+++ bot/utils.py
@@ -169,8 +169,8 @@ async def game_checker():
 
 
 async def scheduler():
-    aioschedule.every(30).seconds.do(training_checker)
-    aioschedule.every(30).seconds.do(game_checker)
+    aioschedule.every(5).seconds.do(training_checker)
+    aioschedule.every(5).seconds.do(game_checker)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
