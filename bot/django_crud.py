be80e9c5ac0d061699b1e1374852b97f3307544e
Shoichii <khayn1992@gmail.com>
2023-10-29 14:46:09 +0300

    fix

--- bot/django_crud.py
+++ bot/django_crud.py
@@ -532,7 +532,9 @@ def make_game_entry(date_time, user_id):
     user = mdl.User.objects.filter(telegram_id=user_id).first()
     journal_entry = mdl.GameJournal.objects.filter(date_time=date_time, user=user).first()
     print('!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    print('СОздание записи в журнале')
+    print('Создание записи в журнале')
+    print(user.name)
+    print(date_time)
     print(journal_entry)
     if not journal_entry:
         new_entry = mdl.GameJournal.objects.create(
